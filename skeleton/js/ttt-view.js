b1167e6f1970e74dc0af5441378d8a6fd9a0a307
Narbe Voskanian <narbevoskanian@gmail.com>
2015-03-25 11:34:47 -0700

    setup board with squares

--- skeleton/js/ttt-view.js
+++ skeleton/js/ttt-view.js
@@ -1,9 +1,12 @@
 (function () {
   if (typeof TTT === "undefined") {
-    window.TTT = {};
+    window.TTT = window.TTT || {};
   }
 
+
   var View = TTT.View = function (game, $el) {
+    this.game = game;
+    this.board = $el
   };
 
   View.prototype.bindEvents = function () {
@@ -13,5 +16,19 @@
   };
 
   View.prototype.setupBoard = function () {
+
+
+    for(var row = 1; row < 4; row++) {
+      var $row = $("<div class='row'></div>");
+      for(var col = 1; col < 4; col++) {
+        var $square = $("<div class='square'></div>");
+        $square.attr("data-colid", col);
+        $square.attr("data-rowid", row);
+        $row.append($square);
+      }
+      this.board.append($row);
+    }
+    // debugger
   };
+
 })();
