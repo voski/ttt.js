36740dd16a2defb12316f8dcb44e3fb8bf1b5fcf
Narbe Voskanian <narbevoskanian@gmail.com>
2015-03-25 15:04:45 -0700

    Add player marks and winner

--- skeleton/js/ttt-view.js
+++ skeleton/js/ttt-view.js
@@ -10,23 +10,48 @@
   };
 
   View.prototype.bindEvents = function () {
+    var that = this;
+    this.board.on("click", ".square", function(pos){
+      var $square = $(event.target);
+      that.makeMove.bind(that)($square);
+    });
   };
 
   View.prototype.makeMove = function ($square) {
+    var position = [$square.data("rowid"), $square.data("colid")];
+    var mark = this.game.currentPlayer;
+    if (!this.game.board.isEmptyPos(position)) {
+      alert("Not an empty square!");
+    } else {
+      this.game.playMove(position);
+      if ( mark === "x") {
+        $square.attr("class", "square selected xmark");
+      } else {
+        $square.attr("class", "square selected omark");
+      }
+      $square.html(mark);
+    }
+    if (this.game.board.isOver()) {
+
+      var $footer = $("footer");
+      var $winMessage = $("<div class='winline'>Congratulations, you are the winner " + mark + "</div>")
+      $footer.append($winMessage);
+      this.board.off();
+    }
   };
 
   View.prototype.setupBoard = function () {
-
-
-    for(var row = 1; row < 4; row++) {
+    for(var row = 0; row < 3; row++) {
       var $row = $("<div class='row'></div>");
-      for(var col = 1; col < 4; col++) {
+      for(var col = 0; col < 3; col++) {
         var $square = $("<div class='square'></div>");
         $square.attr("data-colid", col);
         $square.attr("data-rowid", row);
         $row.append($square);
       }
+
       this.board.append($row);
+      $row.addClass("group");
     }
     // debugger
   };
